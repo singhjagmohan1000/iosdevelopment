ccc9e5822f19fd091762e8b87b3d6a9503a5390c
Jagmoah Singh <singhjagmohan4734@gmail.com>
2018-09-06 01:26:30 -0700

    completing lesson 1

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +22,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in Progress"
+        
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+    }
 }
 
